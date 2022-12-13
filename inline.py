c2c4931e803089d1a0705c9d94a460d72623f67a
unknown <l84328g@gmail.com>
2022-12-14 03:01:27 +0800

    Complete InlineOrderMaker.

--- inline.py
+++ inline.py
@@ -21,18 +21,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 import selenium
 import webdriver_manager
-print(selenium.__version__)
-print(webdriver_manager.__version__)
+print(f'selenium version ({selenium.__version__})')
+print(f'webdriver_manager version ({webdriver_manager.__version__})')
 
 retryCount = 0
 # TODO: button disable condition
 # TODO: auto but slow, click when present button
 # TODO: test internet influence
 # TODO: update selenium version
-
-
-def info_check(order_info):
-    pass
 
 
 def fill_adult(chrome):
@@ -74,23 +70,32 @@ def select_date(chrome, sleep_time):
 
 
 class InlineOrderMaker():
-    def __init__(self, order_info):
-        self.order_info = order_info
+    def __init__(self, order_info: dict, driver_path='./chromedriver', sleep_time: float = 0.5):
+        self.driver_path = driver_path
+        self.update_order_info(order_info, self.driver_path)
+        self.sleep_time = sleep_time
+
+    def get_chrome(self, driver_path, url):
         options = Options()
         options.add_argument("--no-sandbox")
         # options.add_argument("--headless")
-        info_check(order_info)
-        sleep_time = 0.35
 
-        # chrome = webdriver.Chrome(service=ChromeService(
-        #     ChromeDriverManager().install()))
-        self.chrome = webdriver.Chrome('./chromedriver', options=options)
-        # chrome = webdriver.Chrome('./chromedriver', options=options)
-        self.chrome.implicitly_wait(1)
-        self.chrome.get(order_info['url'])
+        chrome = webdriver.Chrome(driver_path, options=options)
+        chrome.implicitly_wait(1)
+        chrome.get(url)
+        return chrome
 
-    def update_order_info(self, order_info):
+    @staticmethod
+    def inspect_order(order_info):
+        if order_info is not None:
+            return True
+
+    def update_order_info(self, order_info: dict, driver_path=None):
         self.order_info = order_info
+        if driver_path is not None:
+            driver_path = self.driver_path
+        assert self.inspect_order(self.order_info), 'Legal input order'
+        self.chrome = self.get_chrome(driver_path, self.order_info['url'])
 
     def refresh(self):
         t1 = time.time()
@@ -99,164 +104,87 @@ class InlineOrderMaker():
         print(f'Refresh time {t2-t1}')
 
     def run(self):
-        self.refresh()
+        try:
+            self.refresh()
 
-# def inlineFill(order_info):
-#     options = Options()
-#     options.add_argument("--no-sandbox")
-#     # options.add_argument("--headless")
-#     info_check(order_info)
-#     sleep_time = 0.35
+            t1 = time.time()
+            select = Select(self.chrome.find_element(
+                By.ID, value="adult-picker"))
+            select.select_by_value(str(order_info['adult']))
+            select = Select(self.chrome.find_element(
+                By.ID, value="kid-picker"))
+            select.select_by_value(str(order_info['kid']))
 
-#     try:
-#         # chrome = webdriver.Chrome(service=ChromeService(
-#         #     ChromeDriverManager().install()))
-#         chrome = webdriver.Chrome('./chromedriver', options=options)
-#         # chrome = webdriver.Chrome('./chromedriver', options=options)
-#         chrome.implicitly_wait(1)
-#         chrome.get(order_info['url'])
-#         t1 = time.time()
-#         chrome.refresh()  # 0.5 ~ 0.7 sec
-#         tt1 = time.time()
-#         print(tt1-t1)
+            t2 = time.time()
+            self.chrome.execute_script(f"window.scrollTo(0, 915)")
+            # TODO: zoom out sol. for show all the elements for clicking -> problem is the location isn't correct
+            # chrome.execute_script("document.body.style.zoom='33%'")
+            sleep(self.sleep_time)
 
-#         # select = Select(chrome.find_element_by_id("book-now"))
+            c = self.chrome.find_element(By.ID, value="date-picker")
+            d = self.chrome.find_element(
+                By.CSS_SELECTOR, f"div[data-date='{order_info['date']}']")
+            c.click()
+            d.click()
 
-#         # # fill in date information
-#         # try:
-#         #     select = Select(chrome.find_element_by_id("date-picker"))
-#         #     select.select_by_index(order_info['adult'])
-#         # except:
-#         #     pass
+            while True:
+                try:
+                    e = self.chrome.find_element(
+                        By.CSS_SELECTOR, f"button[data-cy='book-now-time-slot-box-{order_info['time']}']")
+                    # e = WebDriverWait(self.chrome, 20).until(
+                    #     EC.elementToBeClickable((By.CSS_SELECTOR, f"button[data-cy='book-now-time-slot-box-{order_info['time']}']")))
+                    # WebDriverWait(self.chrome, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@id='brandSlider']/div[1]/div/div/div/img)[50]")))
+                    # element.click()
+                    # sleep(0.7)
+                    e.click()
+                    break
+                except:
+                    pass
 
-#         select = Select(chrome.find_element(By.ID, value="adult-picker"))
-#         select.select_by_value(str(order_info['adult']))
-#         select = Select(chrome.find_element(By.ID, value="kid-picker"))
-#         select.select_by_value(str(order_info['kid']))
+            t3 = time.time()
+            f = self.chrome.find_element(
+                By.CSS_SELECTOR, "button[data-cy='book-now-action-button']")
+            f.click()
 
-#         # # fill in adult information
-#         # try:
-#         #     # tt0 = time.time()
-#         #     select = Select(chrome.find_element(By.ID, value="adult-picker"))
-#         #     # tt1 = time.time()
-#         #     select.select_by_value(str(order_info['adult']))
-#         #     # tt2 = time.time()
-#         #     # print(tt2-tt1)
-#         #     # print(tt1-tt0)
-#         # except:
-#         #     pass
+            # TODO: the clickable situation will be influenced by the page position
+            # TODO: change implicity wait
+            g = self.chrome.find_element(By.ID, value="name")
+            g.send_keys(order_info['name'])
 
-#         # # fill in kid information
-#         # try:
-#         #     select = Select(chrome.find_element(By.ID, value="kid-picker"))
-#         #     select.select_by_value(str(order_info['kid']))
-#         # except NoSuchElementException:
-#         #     print("No kid field!")
+            # # TODO: select gender, need to scroll up
+            # k = WebDriverWait(self.chrome, 20).until(
+            #     EC.presence_of_element_located((By.XPATH, f'//input[@id="gender-{order_info["gender"]}"]')))
+            # k.click()
 
-#         # p1 = Process(target=fill_adult, args={'chrome': chrome})
-#         # p2 = Process(target=fill_adult, args={'chrome': chrome})
-#         # p1.start()
-#         # p2.start()
-#         # p1.join()
-#         # p2.join()
+            h = self.chrome.find_element(By.ID, value="phone")
+            h.send_keys(order_info['phone'])
 
-#         # fill in adult information
-#         # chrome.execute_script("document.body.style.zoom='50%'")
+            submit = self.chrome.find_element(
+                By.CSS_SELECTOR, "button[data-cy='submit']")
+            t4 = time.time()
+            print(f'Time: {t2-t1} sec')
+            print(f'Time: {t3-t2} sec')
+            print(f'Time: {t4-t3} sec')
+            print(20*'-')
+            print(f'Totla Time: {t4-t1} sec')
+            submit.click()
 
-#         # chrome.execute_script("window.scrollTo(0, 0)")
-#         # b = chrome.find_element_by_class_name("sc-dIouRR dlbwaR")
-#         # b = chrome.find_element_by_class_name("sc-dIouRR fMba-De")
-#         # b.click()
+            # TODO: 處理吧沐的confirm botton, data-cy="confirm-house-rule
+            sleep(10)
+            print('Complete!')
 
-#         t2 = time.time()
-#         # p1 = Process(target=select_date, args=(chrome, sleep_time))
-#         # p1.start()
-#         # p1.join()
-
-#         # sleep(sleep_time)
-#         chrome.execute_script(f"window.scrollTo(0, 915)")
-#         # chrome.execute_script("document.body.style.zoom='33%'")
-#         sleep(sleep_time)
-
-#         # c = chrome.find_element(
-#         #     By.CSS_SELECTOR, value="div[id='date-field, [div[id='date-picker']]")
-#         c = chrome.find_element(By.ID, value="date-picker")
-#         # print(c.location)
-#         d = chrome.find_element(
-#             By.CSS_SELECTOR, f"div[data-date='{order_info['date']}']")
-
-#         # chrome.execute_script(f"window.scrollTo(0, {c.location['y']})")
-
-#         # sleep(sleep_time)
-
-#         c.click()
-
-#         # chrome.execute_script(f"window.scrollTo(0, {d.location['y']})")
-#         # sleep(sleep_time)
-#         d.click()
-
-#         e = chrome.find_element(
-#             By.CSS_SELECTOR, f"button[data-cy='book-now-time-slot-box-{order_info['time']}']")
-#         # chrome.execute_script(f"window.scrollTo(0, {e.location['y']})")
-#         # e = WebDriverWait(chrome, 20).until(
-#         #     EC.presence_of_element_located((By.CSS_SELECTOR, f"button[data-cy='book-now-time-slot-box-{order_info['time']}']")))
-#         sleep(sleep_time*2)
-#         # chrome.execute_script(
-#         #     'document.getElementsByTagName("html")[0].style.scrollBehavior = "auto"')
-#         e.click()
-
-#         t3 = time.time()
-#         f = chrome.find_element(
-#             By.CSS_SELECTOR, "button[data-cy='book-now-action-button']")
-#         f.click()
-
-#         # TODO: the clickable situation will be influenced by the page position --> find_by_id?
-#         # TODO: change implicity wait
-#         # g = WebDriverWait(chrome, 20).until(
-#         #     EC.presence_of_element_located((By.XPATH, '//input[@id="name"]')))
-#         g = chrome.find_element(By.ID, value="name")
-#         g.send_keys(order_info['name'])
-
-#         # TODO: select gender
-#         # k = WebDriverWait(chrome, 20).until(
-#         #     EC.presence_of_element_located((By.XPATH, f'//input[@id="gender-{order_info["gender"]}"]')))
-#         # k.click()
-
-#         # h = WebDriverWait(chrome, 20).until(
-#         #     EC.presence_of_element_located((By.XPATH, '//input[@id="phone"]')))
-#         h = chrome.find_element(By.ID, value="phone")
-#         h.send_keys(order_info['phone'])
-
-#         # sleep(sleep_time)
-#         submit = chrome.find_element(
-#             By.CSS_SELECTOR, "button[data-cy='submit']")
-#         t4 = time.time()
-#         # print(f'Time: {t1-t0} sec')
-#         print(f'Time: {t2-t1} sec')
-#         print(f'Time: {t3-t2} sec')
-#         print(f'Time: {t4-t3} sec')
-#         print(20*'-')
-#         print(f'Totla Time: {t4-t1} sec')
-#         submit.click()
-
-#         # TODO: 處理吧沐的confirm botton, data-cy="confirm-house-rule
-
-#         # TODO:
-#         sleep(10)
-#         print('Complete!')
-#         pass
-
-#     except TimeoutException:
-#         chrome.quit()
-#         global retryCount
-#         retryCount += 1
-#         if (retryCount == 2):
-#             print(id+" Write to error log")
-#             retryCount = 0
-#             pass
-#         else:
-#             print(id+" Try again")
-#             inlineFill(id)
+        # TODO: not workable currently
+        except TimeoutException:
+            # self.chrome.quit()
+            global retryCount
+            retryCount += 1
+            if (retryCount == 2):
+                print(" Write to error log")
+                retryCount = 0
+                pass
+            else:
+                print(" Try again")
+                self.run()
 
 
 def readFile():
@@ -285,7 +213,7 @@ if __name__ == "__main__":
     # url = r'https://inline.app/booking/-KO9-zyZTRpTH7LNAe99/-LOcon_dHjl7H4_PR39w?language=zh-tw'
 
     # 酒灑
-    url = r'https://inline.app/booking/-MyeIq6w0WlGH5oRFcd_:inline-live-2/-MyeIqIyLZ5S6ZK_Cke5?language=zh-tw'
+    # url = r'https://inline.app/booking/-MyeIq6w0WlGH5oRFcd_:inline-live-2/-MyeIqIyLZ5S6ZK_Cke5?language=zh-tw'
 
     # 吧沐
     # url = r'https://inline.app/booking/-Mn4FfHBBwA49zeGtMrF:inline-live-2/-Mn4FfRSxcIrDcxydG0c'
@@ -301,20 +229,25 @@ if __name__ == "__main__":
                   'url': url,
                   'date': '2022-12-28',
                   'time': '19-30'}
+    driver_path = './chromedriver'
+    sleep_time = 0.35
 
     start_time = datetime.datetime.today()
     start_time = start_time.replace(
-        hour=11, minute=59, second=59, microsecond=int(6e5))
-    # start_time = datetime.datetime(2022, 12, 14, hour=1, minute=4, second=50)
+        hour=11, minute=59, second=59, microsecond=int(7.5e5))
+    start_time = datetime.datetime(2022, 12, 14, hour=1, minute=4, second=50)
 
-    maker = InlineOrderMaker()
-    maker.update_order(order_info)
+    maker = InlineOrderMaker(
+        order_info=order_info,
+        driver_path=driver_path,
+        sleep_time=sleep_time
+    )
     last_sec = datetime.datetime.now().second
     while True:
         now = datetime.datetime.now()
         if now > start_time:
             print(now)
-            inlineFill(order_info)
+            # inlineFill(order_info)
             maker.run()
             break
 
